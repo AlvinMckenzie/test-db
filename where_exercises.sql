b3af2347082f5fe051c988465add91373f1b39b8
Alvin Mckenzie <mckenzie.a.alvin@gmail.com>
2022-12-07 14:57:06 -0600

    WHERE ex updated

--- where_exercises.sql
+++ where_exercises.sql
@@ -9,7 +9,7 @@ SELECT * FROM employees WHERE last_name LIKE '%q%';
 
 SELECT * FROM employees WHERE (first_name = 'Irana' OR first_name = 'Vidya' OR first_name = 'Maya') AND gender = 'M';
 
-SELECT * FROM employees WHERE last_name LIKE 'E%' OR '%e';
+SELECT * FROM employees WHERE last_name LIKE 'E%' OR last_name LIKE '%e';
 
 SELECT * FROM employees WHERE last_name LIKE 'E%' AND last_name LIKE '%e';
 
